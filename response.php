95e1d9bc73c650a20430bcc3803e9dcc293f8b8d
Jorge M <jlmadrid360@gmail.com>
2016-02-26 16:23:21 -0500

    Inital commit

---
+++ response.php
@@ -0,0 +1,14 @@
+<?
+/*
+Plugin Name: Tpaga - WooCommerce Gateway
+Plugin URI: http://www.tpaga.co/
+Description: Extends WooCommerce by Adding the Authorize.net AIM Gateway.
+Version: 1
+Author: Tpaga
+Author URI: http://www.tpaga.co/
+*/
+add_action('plugins_loaded', 'woocommerce_tpaga_gateway', 0);
+funciton woocommerce_tpaga_gateway() {
+  
+}
+?>
